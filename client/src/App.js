e21debc4f13f09c000ff8652dc02c5b22ba2f67c
Yoan Stoyanov <yoan.stoyanov@nevexis.com>
2017-06-24 13:42:54 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+// import Elemental from 'elemental';
+// import { Button, Alert, Spinner, Modal } from 'elemental';
 import logo from './logo.svg';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LandingPage from './views/LandingPage';
+import Page from './components/Page';
+import RegisterPage from './views/RegisterPage'
+import ForgottenPasswordPage from './views/ForgottenPasswordPage'
 import AllTeams from './views/AllTeams'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header />
-          <section className="section">
-            <div className="container content">
-              <AllTeams />
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                {/*<Page title="Начална страница">*/}
+                    {/*<LandingPage/>*/}
+                {/*</Page>*/}
+
+                <Page title="Забравена парола">
+                    <ForgottenPasswordPage/>
+                </Page>
+                <Footer />
             </div>
-          </section>
-          <Footer />
-      </div>
-    );
-  }
+        );
+    }
+
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <Header />
+    //             <section className="section">
+    //                 <div className="container content">
+    //                     <LandingPage />
+    //                     {/*<RegisterPage/>*/}
+    //                 </div>
+    //             </section>
+    //         </div>
+    //     );
+    // }
 }
 
 export default App;
